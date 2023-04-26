af65ab6c08921b9ffd8fc2c4c4d6012ddf00b04b
LuizFernandoLeao <luiz.leao@sou.inteli.edu.br>
2023-04-26 13:06:59 -0300

    Semana 02 ajustes

--- 03_AUT_EST_EX_OBRIGATORIOS/Semana 2/projeto.db.sql
+++ 03_AUT_EST_EX_OBRIGATORIOS/Semana 2/projeto.db.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS "sobre" (
 	"telefone"	INTEGER NOT NULL,
 	"email"	TEXT NOT NULL,
 	"endereco"	TEXT NOT NULL,
-	FOREIGN KEY("cod_usuario") REFERENCES "carreira"("cod_usuario"),
-	PRIMARY KEY("cod_usuario" AUTOINCREMENT)
+	PRIMARY KEY("cod_usuario" AUTOINCREMENT),
+	FOREIGN KEY("cod_usuario") REFERENCES "carreira"("cod_usuario")
 );
 CREATE TABLE IF NOT EXISTS "caracteristicas" (
 	"cod_usuario"	INTEGER NOT NULL,
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS "projeto" (
 	"data_fim"	NUMERIC NOT NULL,
 	PRIMARY KEY("cod_projeto")
 );
-INSERT INTO "sobre" VALUES (1,'Luiz Fernando Haddad Saad Villaça Leão','18 anos',11973350403,'luiz.leao@sou.inteli.edu.br','Rua ABC, 123');
-INSERT INTO "caracteristicas" VALUES (1,'Inglês avançado (leitura, escrita e conversação) e alemão intermediário','Colaboração e comprometimento');
-INSERT INTO "carreira" VALUES (1,'INTELI','Engenharia da Computação','2023-30-01','Engenheiro da Computação');
-INSERT INTO "projeto" VALUES (1,'Desenvolvimento de jogo digital','2023-30-01','2023-06-04');
-INSERT INTO "projeto" VALUES (2,'Elaboração de aplicação para ambiente web','2023-17-04','2023-23-06');
+INSERT INTO "sobre" VALUES (1,'luiz fernando haddad saad villaca leao','18 anos',11973350403,'luiz.leao@sou.inteli.edu.br','rua abc, 123');
+INSERT INTO "caracteristicas" VALUES (1,'ingles avancado (leitura, escrita e conversação) e alemao intermediario','Colaboracao e comprometimento');
+INSERT INTO "carreira" VALUES (1,'inteli','engenharia da computacao','2023-30-01','engenheiro da computacao');
+INSERT INTO "projeto" VALUES (1,'desenvolvimento de jogo digital','2023-30-01','2023-06-04');
+INSERT INTO "projeto" VALUES (2,'elaboração de aplicação para ambiente web','2023-17-04','2023-23-06');
 COMMIT;
